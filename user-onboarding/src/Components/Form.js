a8f1e934cf08eaffaac1b3b9a9d03a4ca0b56f08
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-19 15:44:58 -0600

    added in error messages if user touches the fields

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import {withFormik, Form, Field, yupToFormErrors} from 'formik';
+import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
-const UserForm = ({values, handleChange}) =>
-{
-    return(
+const UserForm = ({ values, errors, touched }) => {
+    return (
         <div className='User-Form'>
             <Form>
                 <label htmlFor='name'>Name</label>
-                <Field id='name' type='text' name='name'/>
-
+                <Field id='name' type='text' name='name' />
+                {touched.name && errors.name && (<p className='errors'>{errors.name}</p>)}
+                
                 <label htmlFor='email'>Email</label>
-                <Field id='email' type='email' name='email'/>
-
+                <Field id='email' type='email' name='email' />
+                {touched.email && errors.email && (<p className='errors'>{errors.email}</p>)}
+                
                 <label htmlFor='password'>Password</label>
-                <Field id='password' type='password' name='password'/>
-
+                <Field id='password' type='password' name='password' />
+                {touched.password && errors.password && (<p className='errors'>{errors.password}</p>)}
+                
                 <label htmlFor='tos'>Check to accept the Terms of Service</label>
-                <Field id='tos' type='checkbox' name='tos' checked={values.tos}/>
-
+                <Field id='tos' type='checkbox' name='tos' checked={values.tos} />
+                {touched.tos && errors.tos && (<p className='errors'>{errors.tos}</p>)}
+                
                 <button type='submit'>Submit</button>
             </Form>
         </div>
@@ -26,21 +29,20 @@ const UserForm = ({values, handleChange}) =>
 }
 
 const FormikUserForm = withFormik
-({
-    mapPropsToValues({name, email, password, tos})
-    {
-        return {
-            name: name || '',
-            email: email || '',
-            password: password || '',
-            tos: tos || false
-        };
-    },
-    validationSchema: Yup.object().shape({
-        name: Yup.string().required(),
-        email: Yup.string.email().required(),
-        password: Yup.string.required(),
-        tos: Yup.boolean()
-    })
-})(UserForm);
+    ({
+        mapPropsToValues({ name, email, password, tos }) {
+            return {
+                name: name || '',
+                email: email || '',
+                password: password || '',
+                tos: tos || false
+            };
+        },
+        validationSchema: Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().required(),
+            tos: Yup.boolean()
+        })
+    })(UserForm);
 export default FormikUserForm;
