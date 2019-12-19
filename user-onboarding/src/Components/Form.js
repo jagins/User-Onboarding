88945fb947db31fabb85d748219bd58efe8f178e
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-19 15:24:11 -0600

    Form built out and displaying on screen

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import {withFormik} from 'formik';
+import {withFormik, Form, Field} from 'formik';
 
-const UserForm = ({
-})
+const UserForm = ({values, handleChange}) =>
+{
+    return(
+        <div className='User-Form'>
+            <Form>
+                <label htmlFor='name'>Name</label>
+                <Field id='name' type='text' name='name'/>
+
+                <label htmlFor='email'>Email</label>
+                <Field id='email' type='email' name='email'/>
+
+                <label htmlFor='password'>Password</label>
+                <Field id='password' type='password' name='password'/>
+
+                <label htmlFor='tos'>Check to accept the Terms of Service</label>
+                <Field id='tos' type='checkbox' name='tos' checked={values.tos}/>
+
+                <button type='submit'>Submit</button>
+            </Form>
+        </div>
+    );
+}
 
 const FormikUserForm = withFormik
 ({
