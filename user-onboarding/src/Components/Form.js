2f67eef493f3ef630a9775deb4d0f384c358ce96
Joshua Agins <56888002+jagins@users.noreply.github.com>
2019-12-19 15:08:28 -0600

    Set inital values for props

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -4,5 +4,16 @@ import {withFormik} from 'formik';
 const UserForm = ({
 })
 
-const FormikUserForm = withFormik(UserForm);
+const FormikUserForm = withFormik
+({
+    mapPropsToValues({name, email, password, tos})
+    {
+        return {
+            name: '',
+            email: '',
+            password: '',
+            tos: false
+        }
+    }
+})(UserForm);
 export default FormikUserForm;
